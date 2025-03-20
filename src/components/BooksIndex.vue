79d886a7fe0e392b92c4a75b9c8f8d76e72e3683
alejandrojuarez117 <aljuarez117@gmail.com>
2025-03-19 19:07:25 -0600

    added iterative logic to booksindex and api call in content

---
+++ src/components/BooksIndex.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+  props: {
+    books: Array,
+  },
+};
+</script>
+
+<template>
+  <div>
+    <h1>All Books</h1>
+    <div v-for="book in books" v-bind:key="book.id">
+      <h2>Title: {{ book.title }}</h2>
+      <h4>{{ book.author }}</h4>
+      <p>${{ book.price }} <strong>|</strong> {{ book.stock }}</p>
+    </div>
+  </div>
+</template>
+
+<style></style>
+
+
+
+
+<!-- Books: book_id, title, author, price, stock -->
--- src/components/Content.vue
+++ src/components/Content.vue
@@ -1,8 +1,33 @@
-<script></script>
+<script>
+import axios from "axios";
+import BooksIndex from "./BooksIndex.vue"
+
+export default {
+  components: {
+    BooksIndex,
+  },
+  data: function () {
+    return {
+      books: [],
+    };
+  },
+  created: function () {
+    this.handleIndexBooks();
+  },
+  methods: {
+    handleIndexBooks: function () {
+      axios.get("/books.json").then((response) => {
+        console.log("books index", response);
+        this.books = response.data;
+      });
+    },
+  },
+};
+</script>
 
 <template>
   <main>
-    <h1>Welcome to Vue!</h1>
+    <BooksIndex v-bind:books="books" />
   </main>
 </template>
 
