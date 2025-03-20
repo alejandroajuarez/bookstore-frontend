fcba658fa18b3712ebe9fcaa2dd918004520690f
alejandrojuarez117 <aljuarez117@gmail.com>
2025-03-19 19:44:25 -0600

    added create/new action to frontend

---
+++ src/components/BooksNew.vue
@@ -0,0 +1,40 @@
+<script>
+export default {
+  data: function() {
+    return {
+      newBookParams: {},
+    };
+  },
+  methods: {
+    handleSubmit: function () {
+      this.$emit("createBook", this.newBookParams);
+      this.newBookParams = {};
+    },
+  },
+};
+</script>
+
+<template>
+  <div>
+    <h1>New Book Listing</h1>
+    <form v-on:submit.prevent="handleSubmit">
+      <div>
+        Book:
+        <input v-model="newBookParams.title" type="text" />
+      </div>
+      <div>
+        Author:
+        <input v-model="newBookParams.author" type="text" />
+      </div>
+      <div>
+        Price:
+        <input v-model="newBookParams.price" type="number" />
+      </div>
+      <div>
+        Stock:
+        <input v-model="newBookParams.stock" type="number" />
+      </div>
+      <button type="submit">Create Book Listing</button>
+    </form>
+  </div>
+</template>
--- src/components/Content.vue
+++ src/components/Content.vue
@@ -1,10 +1,12 @@
 <script>
 import axios from "axios";
 import BooksIndex from "./BooksIndex.vue"
+import BooksNew from "./BooksNew.vue"
 
 export default {
   components: {
     BooksIndex,
+    BooksNew,
   },
   data: function () {
     return {
@@ -21,12 +23,24 @@ export default {
         this.books = response.data;
       });
     },
+    handleCreateBooks: function (params) {
+      axios
+      .post("/books.json", params)
+      .then((response) => {
+        console.log("books create", response);
+        this.books.push(response.data);
+      })
+      .catch((error) => {
+        console.log("books create error", error.response);
+      });
+    }
   },
 };
 </script>
 
 <template>
   <main>
+    <BooksNew v-on:createBook="handleCreateBooks"/>
     <BooksIndex v-bind:books="books" />
   </main>
 </template>
