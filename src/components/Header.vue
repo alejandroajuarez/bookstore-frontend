3797278df9f9d1c3d2f9bab5d6d5bd1371d03a28
alejandrojuarez117 <aljuarez117@gmail.com>
2025-03-20 19:56:39 -0600

    fixed router on Header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,33 +1,43 @@
-<script></script>
+<script>
+export default {
+  name: "Navbar",
+};
+</script>
 
 <template>
   <header>
-    <nav>
-      <nav class="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
+    <nav class="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
       <div class="container-fluid">
-        <a class="navbar-brand" href="/books">Bookstore</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <router-link class="navbar-brand" to="/books">Bookstore</router-link>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavDropdown"
+          aria-controls="navbarNavDropdown"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNavDropdown">
           <ul class="navbar-nav">
             <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="/books/new">New Book</a>
+              <router-link class="nav-link active" aria-current="page" to="/books/new">New Book</router-link>
             </li>
             <li class="nav-item dropdown">
               <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                Dropdown link
+                Account
               </a>
               <ul class="dropdown-menu">
-                <li><a class="dropdown-item" href="/signup">Signup</a></li>
-                <li><a class="dropdown-item" href="/login">Login</a></li>
-                <li><a class="dropdown-item" href="/logout">Logout</a></li>
+                <li><router-link class="dropdown-item" to="/signup">Signup</router-link></li>
+                <li><router-link class="dropdown-item" to="/login">Login</router-link></li>
+                <li><router-link class="dropdown-item" to="/logout">Logout</router-link></li>
               </ul>
             </li>
           </ul>
         </div>
       </div>
-    </nav>
     </nav>
   </header>
 </template>
